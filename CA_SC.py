ec452d7412860f6221a884640ce427a641f8c73c
Katomasa404 <katomasa216@eis.hokudai.ac.jp>
2025-06-15 17:32:22 +0900

    Add files via upload

--- CA_SC.py
+++ CA_SC.py
@@ -5,7 +5,7 @@ from matplotlib.colors import Normalize
 from scipy.linalg import solve
 from scipy.stats import truncnorm
 
-plt.rcParams['font.family'] = ['Yu Gothic', 'Meiryo', 'sans-serif']
+plt.rcParams['font.family'] = ['sans-serif', 'Yu Gothic', 'Meiryo']
 
 # 固定パラメータ
 dx = dy = 1 
